3b8b3cfd1502ec666632eb2c7a5107f1626a43e3
Antonin Lenfant <antonin.lenfant@isep.fr>
2012-11-11 20:58:11 +0100

    Basis of the app

---
+++ IMCIapp/src/com/example/imciapp/Database.java
@@ -0,0 +1,39 @@
+package com.example.imciapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+;
+
+public class Database extends SQLiteAssetHelper {
+
+	private static final String DATABASE_NAME = "database";
+	private static final int DATABASE_VERSION = 1;
+
+	public Database(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	public Cursor getSigns() {
+		SQLiteDatabase db = getReadableDatabase();
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+		/*
+		 * String [] sqlSelect = {"0 id", "illness_id", "type", "key",
+		 * "question"}; String sqlTables = "Employees";
+		 * 
+		 * qb.setTables("signs"); Cursor c = qb.query(db, sqlSelect, null, null,
+		 * null, null, null);
+		 */
+		Cursor c = db.rawQuery("SELECT `illness_id`, `illnesses`.`key` `illness_key`, `name`, `type`, `signs`.`key` `sign_key`, `question`, `values` FROM `illnesses` "
+						+ "INNER JOIN `signs` ON `signs`.`illness_id` = `illnesses`.`id` "
+						+ "WHERE `signs`.`age_group` = 2 "
+						+ "ORDER BY `illnesses`.`sequence`", new String[0]);
+		c.moveToFirst();
+		return c;
+	}
+}
